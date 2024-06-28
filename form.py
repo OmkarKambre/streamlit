37140c37567b870d7f8f535f7ac0b678b11ff837
unknown <okambre2@gmail.com>
2024-06-28 23:19:55 +0530

    Form Update

--- form.py
+++ form.py
@@ -1,13 +1,13 @@
 import streamlit as st
+st.image('inmac.png')
+st.title("Add Engineer to DB")
 
 with st.form(key="form1"):
-    name = st.text_input(label="Enter name")
-    pno = st.text_input(label="Enter phone number")
-    email = st.text_input(label="Enter email")
-    interest = ["ML", "AI"]
-    ml_interest = st.checkbox(label="ML", value=False)
-    ai_interest = st.checkbox(label="AI", value=False)
-    location = st.text_input(label="Enter location")
+    name = st.text_input(label="Name*")
+    pno = st.text_input(label="Phone Number*")
+    email = st.text_input(label="Email ID*")
+    field = st.toggle("Field Engineer")
+    location = st.text_input(label="Location")
     domain_option = [
         'IT', 
         'CS'
@@ -16,9 +16,12 @@ with st.form(key="form1"):
     submit = st.form_submit_button(label="Submit")
 
 if submit:
-    st.write(f"Name: {name}")
-    st.write(f"Phone Number: {pno}")
-    st.write(f"Email: {email}")
-    st.write(f"Field of Interest: {ml_interest, ai_interest}")
-    st.write(f"Location: {location}")
-    st.write(f"Domain: {domain}")
+    if not name or not pno or not email:
+        st.warning("Please fill out the Name, Phone Number, and Email fields.")
+    else:
+        st.write(f"Name: {name}")
+        st.write(f"Phone Number: {pno}")
+        st.write(f"Email: {email}")
+        st.write(f"Field of Interest: {ml_interest, ai_interest}")
+        st.write(f"Location: {location}")
+        st.write(f"Domain: {domain}")
